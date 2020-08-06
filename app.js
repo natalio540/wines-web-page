cf8d2725676e4b448cfeb3a95860ccd0acf49b7c
natogua <natalio.guardeno@gmail.com>
2020-08-06 17:53:31 -0300

    pagina hecha responsive

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("mapa");
+
+    // scroll reveal containers
+    ScrollReveal().reveal('.letra', { easing: 'ease-in' });
+    ScrollReveal().reveal('#info1', { easing: 'ease-in' });
+    ScrollReveal().reveal('#info2', { easing: 'ease-in' });
+    ScrollReveal().reveal('.products-container', { easing: 'ease-in' });
+    ScrollReveal().reveal('#end-container', { easing: 'ease-in' });
+    
+ 
+    // button animation
+    $("#changeTo2").click(function () {
+        $("#product1").fadeOut(1000)
+        $("#product2").fadeIn(1000)
+        
+    });
+
+    $("#changeTo1").click(function () {
+        $("#product1").fadeIn(1000)
+        $("#product2").fadeOut(1000)
+        
+    });
+
+    // back to top button
+    $('document').ready(function($){
+        let top =$('.back-to-top');
+        top.click(function(e){
+            e.preventDefault();
+            $('html,body').animate({scrollTop:0},500);
+
+        });
+    })
+       
